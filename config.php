f32812671be6e40630cefdc28b46ec497e8b1c94
teamteerapat <139048607+teamteerapat@users.noreply.github.com>
2026-02-03 15:22:00 +0700

    change port from 3306 --> 3307

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 // Connect to server and select database.
 define('servername', 'localhost:3307'); //เปลี่ยน port นะครับ
+//ผมเปลี่ยน พอรืตจาก default 3306 -> 3307
 define('username', 'root');
 define('password', '');
 define('dbname', 'myhotel');
